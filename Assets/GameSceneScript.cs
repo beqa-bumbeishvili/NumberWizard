078d46b2762a59eef8667cd781ec1a4f3febe168
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-02-11 20:02:54 +0400

    write game algorithm

--- Assets/GameSceneScript.cs
+++ Assets/GameSceneScript.cs
@@ -1,12 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameSceneScript : MonoBehaviour
 {
+    public Text RenderedNumber;
+    int min;
+    int max;
+    int guess;
+
+    public void Start()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+        RenderedNumber.text = guess+"?";
+    }
+    
     public void GoToStartScene()
     {
         SceneManager.LoadScene("Start");
     }
+
+    public void RightAnswerClick()
+    {
+        SceneManager.LoadScene("Win");
+    }
+
+    public void HigherTextBtnClick()
+    {
+        min = guess;
+        guess = (min + max) / 2;
+        RenderedNumber.text = guess+"?";
+    }
+
+    public void LowerTextBtnClick()
+    {
+        max = guess;
+        guess = (min + max) / 2;
+        RenderedNumber.text = guess + "?";
+    }
+
 }
---
+++ Assets/PlayAgainScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgainScript : MonoBehaviour {
+
+    public void RedirectToStartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+}
