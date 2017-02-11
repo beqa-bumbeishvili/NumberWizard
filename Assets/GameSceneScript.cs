ab6d3c3d3cfbad1fe813926b2bae5ae3144012d6
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-02-11 18:28:12 +0400

    initial commit

---
+++ Assets/GameSceneScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneScript : MonoBehaviour
+{
+    public void GoToStartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
